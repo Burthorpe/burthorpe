6adc690467ad0f72f530e5f752867d6221590027
Wade Urry <wader@iwader.co.uk>
2014-05-10 12:33:26 +0100

    Only run composer once

--- cookbooks/burthorpe/recipes/default.rb
+++ cookbooks/burthorpe/recipes/default.rb
@@ -4,10 +4,3 @@ composer_project "/vagrant" do
   quiet false
   action [:install, :dump_autoload]
 end
-
-composer_project "/vagrant" do
-  dev true
-  run_as "vagrant"
-  quiet false
-  action [:install, :dump_autoload]
-end
